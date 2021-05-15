28ee26604406a55b19e04797af36b8d462e030c8
anamilicevic <62260334+anamilicevic@users.noreply.github.com>
2021-05-15 13:55:48 +0200

    login/signup implementacija
    zavrsen sistem za autentifikaciju

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ const lobbyRoutes = require('./routes/lobby');
 const User = require('./models/user');
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+
 mongoose.connect('mongodb://localhost:27017/login-app-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -17,6 +22,8 @@ mongoose.connect('mongodb://localhost:27017/login-app-db', {
 })
 
 const app = express();
+app.use(bodyParser.json());
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -38,6 +45,67 @@ app.use(function(error, req, res, next) {
     });
 });
 
+app.post('/api/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
+
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
+
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
+
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+
+	res.json({ status: 'ok' })
+})
+
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
+})
 
 
 module.exports = app
